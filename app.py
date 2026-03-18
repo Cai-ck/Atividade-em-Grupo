84788b44226f281d8f98d582af2b7c236443dd12
Raphael_Issa <raphaelbiof0@gmail.com>
2026-03-18 15:33:15 -0300

    Implantando logica mais Streamlit básico!

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import polars as pl
-
+import streamlit as st
 
 df = pl.read_csv("Dados/vendas_ficticias.csv") 
 
 total_vendido = df["quantidade"].sum()
 
-print(f"O total de produtos vendidos é: {total_vendido}")
+st.write(f"O total de produtos vendidos é: {total_vendido}")   
